e1ac59dd6e0373d79372c0593c61fd13d2d8597b
zbluebugz <51784903+zbluebugz@users.noreply.github.com>
2023-07-16 18:21:26 +0100

    Create background.js

---
+++ context-menu/chromium/background_scripts/background.js
@@ -0,0 +1,36 @@
+// -- not all browsers have the same context menu api object.
+// -- so, detect which one the browser supports ...
+
+let contextMenuAPI;
+if (typeof browser !== "undefined" && browser.contextMenus) {
+  // -- Firefox
+  contextMenuAPI = browser;
+  // console.info('using browser object');
+}
+else if (typeof chrome !== "undefined" && chrome.contextMenus) {
+  // -- Chrome or Opera
+  contextMenuAPI = chrome;
+  // console.info('using chrome object');
+}
+else {
+  console.log('Context menu is not supported in this browser');
+}
+
+if (contextMenuAPI) {
+  const MENU_ID = 'turkey-opinion';
+  // -- populate the context menu panel
+  contextMenuAPI.contextMenus.create({
+    id: MENU_ID,
+    title: "Gobble gobble"
+  });
+
+  // -- listen and run content script
+  contextMenuAPI.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === MENU_ID) {
+      contextMenuAPI.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ["/content_scripts/ask-turkey.js"]
+      });
+    }
+  });
+}
